3fedbe096cf6c66151afdffc13424f5c95d154df
JAGNG <2484285962@qq.com>
2022-10-05 03:24:33 +0800

    初始提交

---
+++ admin/src/main/java/com/jagng/web/controller/system/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.jagng.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.jagng.common.core.controller.BaseController;
+import com.jagng.common.core.domain.AjaxResult;
+import com.jagng.common.core.domain.model.RegisterBody;
+import com.jagng.common.utils.StringUtils;
+import com.jagng.framework.web.service.SysRegisterService;
+import com.jagng.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author ruoyi
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
---
+++ common/src/main/java/com/jagng/common/core/domain/model/RegisterBody.java
@@ -0,0 +1,11 @@
+package com.jagng.common.core.domain.model;
+
+/**
+ * 用户注册对象
+ * 
+ * @author ruoyi
+ */
+public class RegisterBody extends LoginBody
+{
+
+}
